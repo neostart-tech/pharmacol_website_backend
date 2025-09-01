e9abeeab32a3d606317375590d0240a84ffe0664
unknown <ebenezerafantsao@gmail.com>
2025-09-01 11:00:20 +0000

    Second correction

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -414,7 +414,7 @@ Fondée en 1996 par Abel ACOLATSE, PHARMACOL est une agence spécialisée dans l
             <a href="" class="flex flex-col gap-4 bg-white rounded-lg shadow-md p-5 hover:bg-[#437305] hover:cursor-pointer hover:text-white transition-all duration-500">
                 <img src="images/Page index/7v.png" alt="img" class="w-20 h-20 md:w-24 md:h-24 mx-auto">
                 <div class="font-bold text-lg md:text-2xl text-center">Reporting Pharmacovigilance</div>
-                <div class="font-normal text-sm md:text-base text-center">Phasellus neque nibh, cursus<br>ullamcorper at.</div>
+                <div class="font-normal text-sm md:text-base text-center">Analyse et reporting des événements indésirables médicaux.</div>
             </a>
             <a href="" class="flex flex-col gap-4 bg-white rounded-lg shadow-md p-5 hover:bg-[#437305] hover:cursor-pointer hover:text-white transition-all duration-500">
                 <img src="images/Page index/8v.png" alt="img" class="w-20 h-20 md:w-24 md:h-24 mx-auto">
